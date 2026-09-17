1685131db4c8f5c61886b637440b53dba749e6cb
jorge <jorgequentaoliva005@gmail.com>
2026-09-17 10:16:58 -0500

    sistema de asistencias

---
+++ sistema-asistencias/app/Models/Usuario.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Usuario extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'codigo',
+        'nombre',
+        'carrera_id',
+        'rol',
+        'modalidad',
+        'activo',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'activo' => 'boolean',
+            'password' => 'hashed',
+        ];
+    }
+
+    // Relaciones
+    public function carrera()
+    {
+        return $this->belongsTo(Carrera::class);
+    }
+
+    public function asistencias()
+    {
+        return $this->hasMany(Asistencia::class);
+    }
+
+    public function sesionesGeneradas()
+    {
+        return $this->hasMany(SesionRemota::class, 'generado_por');
+    }
+}
